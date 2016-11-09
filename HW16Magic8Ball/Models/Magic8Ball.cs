885610da79d7d70e2ca9f323ff418d15f9009b35
dmeraz <darin.meraz@gmail.com>
2016-11-08 23:06:09 -0500

    added code to fix StreamReader not working, commented out for test

--- HW16Magic8Ball/Models/Magic8Ball.cs
+++ HW16Magic8Ball/Models/Magic8Ball.cs
@@ -9,23 +9,25 @@ namespace HW16Magic8Ball.Models
 {
     public class Magic8Ball
     {
-        //create an array of strings for responses from .txt file
+        ////Open StreamReader
+        //StreamReader magic8ballanswers = new StreamReader("..\\..\\..\\magic8ballanswers.txt");
 
-        StreamReader magic8ballanswers = new StreamReader("..\\..\\..\\magic8ballanswers.txt");
-        //string responses;
-
-        //get random number
+        ////Store each line of text (a prediction) in a variable
+        //string responses = magic8ballanswers.ReadLine();
 
         public string getResponse()
         {
-            
-            string[] responses = { magic8ballanswers.ReadLine() };
+            //create an array of strings for responses from .txt file
+            //while (magic8ballanswers != null)
+            {
+                string[] responses = { "Prediction1", "Prediction2", "Prediction3" };
 
-            //get random controller object
-            Random randomNum = new Random();
-            int num = randomNum.Next(0, responses.Length); //may be int or var
-            return responses[num];
-
+                //get random number
+                //get random controller object
+                Random randomNum = new Random();
+                int num = randomNum.Next(0, responses.Length); //may be int or var
+                return responses[num];
+            }
         }
     }
 }
