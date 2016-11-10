060dfb90492c2c1d4589fbd6941663f0381dbbaa
dmeraz <darin.meraz@gmail.com>
2016-11-09 20:43:50 -0500

    attempt to use StreamReader CO'ed, reverted to hard-coded array, changed image size and text color in Site.css, WORKS

--- HW16Magic8Ball/Controllers/HomeController.cs
+++ HW16Magic8Ball/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//using System.Threading.Tasks;
+//using System.IO;
 
 namespace HW16Magic8Ball.Controllers
 {
--- HW16Magic8Ball/Models/Magic8Ball.cs
+++ HW16Magic8Ball/Models/Magic8Ball.cs
@@ -2,43 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Threading.Tasks;
-using System.IO;
+//using System.Threading.Tasks;
+//using System.IO;
 
 namespace HW16Magic8Ball.Models
 {
     public class Magic8Ball
     {
         //Open StreamReader
-
-        StreamReader magic8ballanswers = new StreamReader("C:\\Users\\WeCanCodeIT\\Documents\\Visual Studio 2015\\Projects\\HW16Magic8Ball\\magic8ballanswers.txt");
+        //StreamReader magic8ballanswers = new StreamReader("C:\\Users\\WeCanCodeIT\\Documents\\Visual Studio 2015\\Projects\\HW16Magic8Ball\\magic8ballanswers.txt");
 
         //Store each line of text (a prediction) in a variable
         //string responses; No longer needed for List
-        int num;
+        //int num;
 
         public string getResponse()
         {
-            //create a list of strings for responses from .txt file
-            List<string> responses = new List<string>();
+            //Array of Predicitons
+            string[] responses = { "It is certain", "You may rely on it", "Yes", "Better not tell you now", "My reply is no" };
 
-            //fill array with responses
-            using (magic8ballanswers)
-            {
-                while (magic8ballanswers != null)
-                {
-                    responses.Add(magic8ballanswers.ReadLine());
-                }
-            }
-
-            //convert list to array
-
-            string[] responsesArray = responses.ToArray();
             //get random number
             //get random controller object
             Random randomNum = new Random();
-            num = randomNum.Next(0, responsesArray.Length);
-            return responsesArray[num];
+            int num = randomNum.Next(0, responses.Length); //may be int or var
+            return responses[num];
+
+            //Attempt to use StreamReader
+            ////create a list of strings for responses from .txt file
+            //List<string> responses = new List<string>();
+
+            //int capacity = responses.Capacity;
+
+            ////fill array with responses
+            //using (magic8ballanswers)
+            //{
+            //    while (magic8ballanswers != null)
+            //    {
+            //        responses.Add(magic8ballanswers.ReadLine());
+            //        if (responses.Capacity > capacity)
+            //        {
+            //            capacity = responses.Capacity;
+            //        }
+            //    }
+            //}
+
+            ////convert list to array
+            ////string[] responsesArray = responses.ToArray();
+
+            ////get random number
+            ////get random controller object
+            //Random randomNum = new Random();
+            //num = randomNum.Next(0, responses.Count);
+            //return responses[num];
         }
     }
 }
